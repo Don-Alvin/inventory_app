9e0b040adcd50a0bc3ddf6e66426f0a3c1339667
Don-Alvin <alvindon41@gmail.com>
2023-01-30 15:58:18 +0300

    Folder setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
+
+// Middlewares
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
+app.get("/", (req, res) => {
+	res.send("Home Page");
+});
+
+module.exports = app;
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,17 +4,37 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = require("./app");
+const { connect } = require("http2");
 const PORT = process.env.PORT || 8000;
+const server = http.createServer(app);
 
 // Connect MongoDB
 dotenv.config({ path: "./.env" });
 const source = process.env.MONGO_URI;
 mongoose.set("strictQuery", false);
-mongoose
-	.connect(source)
-	.then(() => {
-		console.log("Connected to DB");
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+// mongoose
+// 	.connect(source)
+// 	.then(() => {
+// 		server.listen(PORT, () => {
+// 			console.log("Connected to DB");
+// 			console.log(`Server running on port ${PORT}...`);
+// 		});
+// 	})
+// 	.catch((err) => {
+// 		console.log(err);
+// 	});
+
+const startServer = async () => {
+	try {
+		await mongoose.connect(source);
+
+		server.listen(PORT, () => {
+			console.log("Connected to DB");
+			console.log(`Server running on port ${PORT}...`);
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+startServer();
